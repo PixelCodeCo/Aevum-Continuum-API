36dc4f9debfefe253f001a538507c5cdd87b156d
Jack Tubby <jack200034@hotmail.com>
2026-01-01 22:33:53 +0000

    Delete unnecessary route

--- app/main.py
+++ app/main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from app.routes.auth import router as auth_router
 from app.routes.events import router as events_router
 from app.routes.categories import router as category_router
-from app.routes.enums import router as enums_router
 from app.routes.regions import router as regions_router
 from app.routes.relationships import router as relationships_router
 from app.routes.sources import router as sources_router
@@ -18,7 +17,6 @@ def health_check():
 app.include_router(auth_router)
 app.include_router(events_router)
 app.include_router(category_router)
-app.include_router(enums_router)
 app.include_router(regions_router)
 app.include_router(relationships_router)
 app.include_router(sources_router)
