b9abcd71b52d549a3acdae9e19e1818f4871115c
Jack Tubby <jack200034@hotmail.com>
2026-01-01 22:09:55 +0000

    Complete events handlers

--- app/routes/events.py
+++ app/routes/events.py
@@ -1,23 +1,46 @@
 from fastapi import APIRouter
+from fastapi import Depends
+from dependencies import get_supabase
+from schemas import EventCreate, EventUpdate
+from uuid import UUID
 
 router = APIRouter(prefix="/events", tags=["events"])
 
 @router.get("")
-def get_events():
-    return "getAll"
+def get_events(supabase = Depends(get_supabase)):
+    response = supabase.table("events").select("*").execute()
+    return response.data
 
 @router.get("/{id}")
-def get_event(id: str):
-    return f"get {id}"
+def get_event(id: UUID, supabase = Depends(get_supabase)):
+    response = supabase.table("events").select("*").eq("id", str(id)).execute()
+    return response.data
 
 @router.post("")
-def create_event():
-    return "post"
+def create_event(data: EventCreate, supabase = Depends(get_supabase)):
+    response = (
+        supabase.table("events")
+        .insert(data.model_dump())
+        .execute()
+    )
+    return response.data
 
 @router.patch("/{id}")
-def update_event(id: str):
-    return f"patch {id}"
+def update_event(data: EventUpdate, id: UUID, supabase = Depends(get_supabase)):
+    response = (
+        supabase.table("events")
+        .update(data.model_dump(exclude_unset=True))
+        .eq("id", str(id))
+        .execute()
+    )
+    return response.data
 
 @router.delete("/{id}")
-def delete_event(id: str):
-    return f"delete {id}"
+def delete_event(id: UUID, supabase = Depends(get_supabase)):
+    response = (
+        supabase.table("events")
+        .delete()
+        .eq("id", str(id))
+        .execute()
+    )
+    return response.data
